5024f74b52966ac78312544cb0412273be0c6d17
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-09-03 13:28:29 +0200

    serverless test

---
+++ app.js
@@ -0,0 +1,45 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ */
+export default (app) => {
+    // Your code here
+    app.log.info("Yay, the app was loaded!");
+  
+    // when an issue is opened, greet the author
+    app.on("issues.opened", async (context) => {
+      const user = context.payload.issue.user.login;
+      const issueComment = context.issue({
+        body: "Hello @" + user + "! Thanks for opening this issue.",
+      });
+  
+      return context.octokit.issues.createComment(issueComment);
+    });
+  
+    app.on("repository_dispatch", async (context) => {
+      context.log.info("repository_dispatch event received");
+  
+      const { action, repository, client_payload } = context.payload;
+      const releaseTag = client_payload.release_tag;
+  
+      context.log.info(`repository_dispatch action: ${action}, release_tag: ${releaseTag}, from repository: ${repository.full_name}`);
+      // Handle the repository_dispatch event with releaseTag
+    });
+  
+    app.on("workflow_dispatch", async (context) => {
+      context.log.info("workflow_dispatch event received");
+  
+      const { inputs } = context.payload;
+      const releaseTag = inputs.release_tag;
+  
+      context.log.info(`workflow_dispatch manually triggered with release_tag: ${releaseTag}`);
+      // Handle the workflow_dispatch event with releaseTag
+    });
+  
+  
+    // For more information on building apps:
+    // https://probot.github.io/docs/
+  
+    // To get your app running against GitHub, see:
+    // https://probot.github.io/docs/development/
+  };
---
+++ code_operations.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const execPromise = require('util').promisify(require('child_process').exec);
+
+async function updateBindings(context, owner, repo, branch_name, tag, scriptName) {
+  const { data: gencle_data } = await context.github.repos.get({
+    owner,
+    repo: 'gencle',
+  });
+  const gencle_dir = path.join('/tmp', 'gencle');
+  await execPromise(`git clone ${gencle_data.clone_url} ${gencle_dir}`);
+  const { data: repo_data } = await context.github.repos.get({
+    owner,
+    repo,
+  });
+  const repo_dir = path.join('/tmp', repo);
+  await execPromise(`git clone ${repo_data.clone_url} ${repo_dir}`);
+  await execPromise(`cd ${repo_dir} && git fetch && git checkout ${branch_name}`);
+  await execPromise(`python3 ${gencle_dir}/${scriptName} ${repo_dir} ${tag}`);
+  const { stdout: diff } = await execPromise(`cd ${repo_dir} && git diff`);
+  if (diff) {
+    await execPromise(`cd ${repo_dir} && git add . && git commit -m "Update to ${tag}" && git push`);
+  } else {
+    console.log("No changes made by the update script");
+  }
+  // Clean up
+  await execPromise(`rm -rf ${gencle_dir}`);
+  await execPromise(`rm -rf ${repo_dir}`);
+}
+
+module.exports = {
+    updateBindings
+};  
