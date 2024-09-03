f12327be5be045a0b476767c582d6a6544138d64
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-09-03 13:51:53 +0200

    try netlify function

---
+++ netlify/functions/webhooks.js
@@ -0,0 +1,10 @@
+// index.js
+import {
+    createLambdaFunction,
+    createProbot,
+  } from "@probot/adapter-aws-lambda-serverless";
+  import app from "../../app";
+  
+  export const handler = createLambdaFunction(app, {
+    probot: createProbot(),
+  });
